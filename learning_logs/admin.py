488c544494f06728c9d46a820a35d7c0798ed458
Marcos <sirluamark@gmail.com>
2024-07-20 23:36:01 +0200

    Render

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Topic, Entry
 
+admin.site.site_header = 'Learning Log Admin'
+admin.site.site_title = 'Learning Log Admin'
+admin.site.index_title = 'Bienvenido a la administración de Learning Log'
+admin.site.site_url = '/'
+
 admin.site.register(Topic)
 admin.site.register(Entry)
